0fcece9fc0b5cc43f844ade25250aa85b45d4777
sensei <cristiantomat@gmail.com>
2018-10-24 17:04:23 -0400

    Primer commit

---
+++ 1-holamundo.py
@@ -0,0 +1,3 @@
+print("Hola Mundo")
+
+"Pytho se caracteriza por la filosofia de 'menos es mejor'"
---
+++ 2-strings_y_variables.py
@@ -0,0 +1,14 @@
+nombre_del_personaje = "Jacobo"
+edad_del_personaje = 40
+
+"""En python no definen las variables, estas se definen por si
+mismas al momento de recibir cualquier tipo de dato
+"""
+
+print(nombre_del_personaje + " es una persona de "
+      + str(edad_del_personaje) + " años de edad ")
+
+"""Por si solo, no es posible mostrar numeros y caracteres
+el mismo 'print'. Sin embargo, con la funcion str() podemos
+traducir los enteros o decimales a cadenas de caracteres
+"""
