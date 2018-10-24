a1db4d5e0a2a1c4dfc8d7cf6c1d031808bd1228f
sensei <cristiantomat@gmail.com>
2018-10-24 17:09:04 -0400

    Corrigiendo algunos errores de tipeo

--- 2-strings_y_variables.py
+++ 2-strings_y_variables.py
@@ -1,7 +1,7 @@
 nombre_del_personaje = "Jacobo"
 edad_del_personaje = 40
 
-"""En python no definen las variables, estas se definen por si
+"""En python no se definen las variables, estas se definen por si
 mismas al momento de recibir cualquier tipo de dato
 """
 
@@ -9,6 +9,6 @@ print(nombre_del_personaje + " es una persona de "
       + str(edad_del_personaje) + " años de edad ")
 
 """Por si solo, no es posible mostrar numeros y caracteres
-el mismo 'print'. Sin embargo, con la funcion str() podemos
+en el mismo 'print'. Sin embargo, con la funcion str() podemos
 traducir los enteros o decimales a cadenas de caracteres
 """
